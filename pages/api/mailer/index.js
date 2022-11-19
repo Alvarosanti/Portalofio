af546a8b863897e5338504b116e1675567e02ca1
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2022-11-18 20:00:58 -0500

    update api mailer no function

--- pages/api/mailer/index.js
+++ pages/api/mailer/index.js
@@ -5,35 +5,32 @@ const nodemailer = require('nodemailer');
 export default async function mailer(req, res) {
 	try {
 		const body = JSON.parse(req.body);
-		const sendEmail = async () => {
-			const config = {
-				host: 'smtp.gmail.com',
-				post: 465,
-				auth: {
-					user: 'alvaroportfoliomail@gmail.com',
-					pass: process.env.password
-				},
-				secure: true
-			};
+		const config = {
+			host: 'smtp.gmail.com',
+			post: 465,
+			auth: {
+				user: 'alvaroportfoliomail@gmail.com',
+				pass: process.env.password
+			},
+			secure: true
+		};
 
-			const mensaje = {
-				from: 'alvarosantisteban56@gmail.com',
-				to: 'alvaroportfoliomail@gmail.com',
-				subject: `Contacto - Portfolio - Hola soy, ${body.name} ✈️🛫`,
-				text: `\nNombre: ${body.name}`,
-				html: `
+		const mensaje = {
+			from: 'alvarosantisteban56@gmail.com',
+			to: 'alvaroportfoliomail@gmail.com',
+			subject: `Contacto - Portfolio - Hola soy, ${body.name} ✈️🛫`,
+			text: `\nNombre: ${body.name}`,
+			html: `
 					<p>Mi nombre es, <strong>${body.name}</strong></p> 
 					<p>Mi correo es: <strong>${body.email}</strong></p> 
 					<p>Mensaje: <strong>${body.mesagge}</strong></p>
 					`
-			};
-
-			const transport = nodemailer.createTransport(config);
-
-			const info = await transport.sendMail(mensaje);
-			console.log(info);
 		};
-		sendEmail();
+
+		const transport = nodemailer.createTransport(config);
+
+		const info = await transport.sendMail(mensaje);
+		console.log(info);
 		res.status(200).json('mail sent');
 	} catch (error) {
 		res.status(404).json('mail error');
