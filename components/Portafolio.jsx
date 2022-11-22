971715cd9908dd9c3197b453920ef7f5a06cc508
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2022-11-22 17:10:05 -0500

    images portfolio

--- components/Portafolio.jsx
+++ components/Portafolio.jsx
@@ -15,14 +15,14 @@ export default function Portafolio() {
 							<Image
 								width="470"
 								height="470"
-								src="https://res.cloudinary.com/dzsitpxzw/image/upload/v1668206086/portafolio/wall2_smykx8.jpg"
+								src="https://res.cloudinary.com/dzsitpxzw/image/upload/e_improve,w_300,h_450,c_thumb,g_auto/v1669152904/portafolio/marinera_sokwte.jpg"
 								className="img-fluid"
 								alt={'Academia Marinera'}
 							/>
 							<div className="content-holder">
 								<a
 									className="img-popup"
-									href="https://res.cloudinary.com/dzsitpxzw/image/upload/v1668206086/portafolio/wall2_smykx8.jpg"
+									href="https://res.cloudinary.com/dzsitpxzw/image/upload/e_improve,w_300,h_450,c_thumb,g_auto/v1669152904/portafolio/marinera_sokwte.jp"
 								/>
 								<div className="text-holder">
 									<h6 className="title">Academia de Marinera</h6>
@@ -36,14 +36,14 @@ export default function Portafolio() {
 							<Image
 								width="470"
 								height="470"
-								src="https://res.cloudinary.com/dzsitpxzw/image/upload/v1668206086/portafolio/wall2_smykx8.jpg"
+								src="https://res.cloudinary.com/dzsitpxzw/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1669153228/portafolio/logo1_shvwp0.svg"
 								className="img-fluid"
 								alt={'Decormolduras SAC'}
 							/>
 							<div className="content-holder">
 								<a
 									className="img-popup"
-									href="https://res.cloudinary.com/dzsitpxzw/image/upload/v1668206086/portafolio/wall2_smykx8.jpg"
+									href="https://res.cloudinary.com/dzsitpxzw/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1669153228/portafolio/logo1_shvwp0.svg"
 								/>
 								<div className="text-holder">
 									<h6 className="title">Empresa DECORMOLDURAS & ROSETONES S.A.C.</h6>
@@ -58,14 +58,14 @@ export default function Portafolio() {
 							<Image
 								width="470"
 								height="470"
-								src="https://res.cloudinary.com/dzsitpxzw/image/upload/v1668206086/portafolio/wall2_smykx8.jpg"
+								src="https://res.cloudinary.com/dzsitpxzw/image/upload/e_improve,w_300,h_450,c_thumb,g_auto/v1669153357/portafolio/avicola_wqcnh8.jpg"
 								className="img-fluid"
 								alt={'AvicolaN'}
 							/>
 							<div className="content-holder">
 								<a
 									className="img-popup"
-									href="https://res.cloudinary.com/dzsitpxzw/image/upload/v1668206086/portafolio/wall2_smykx8.jpg"
+									href="https://res.cloudinary.com/dzsitpxzw/image/upload/e_improve,w_300,h_450,c_thumb,g_auto/v1669153357/portafolio/avicola_wqcnh8.jpg"
 								/>
 								<div className="text-holder">
 									<h6 className="title">AV&Iacute;COLA</h6>
@@ -79,14 +79,14 @@ export default function Portafolio() {
 							<Image
 								width="470"
 								height="470"
-								src="https://res.cloudinary.com/dzsitpxzw/image/upload/v1668206086/portafolio/wall2_smykx8.jpg"
+								src="https://res.cloudinary.com/dzsitpxzw/image/upload/e_improve,w_300,h_450,c_thumb,g_auto/v1669153596/portafolio/pasteleria_qjltau.jpg"
 								className="img-fluid"
 								alt={'Pasteleria'}
 							/>
 							<div className="content-holder">
 								<a
 									className="img-popup"
-									href="https://res.cloudinary.com/dzsitpxzw/image/upload/v1668206086/portafolio/wall2_smykx8.jpg "
+									href="https://res.cloudinary.com/dzsitpxzw/image/upload/e_improve,w_300,h_450,c_thumb,g_auto/v1669153596/portafolio/pasteleria_qjltau.jpg"
 								/>
 								<div className="text-holder">
 									<h6 className="title">Pasteler&iacute;a</h6>
