9cd94c9857f5ff846c3eb5bf2530fef7c1b7eda5
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2022-11-23 13:02:56 -0500

    fix head

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,10 @@ import '../styles/globals.css';
 export default function RootLayout({ children }) {
 	return (
 		<html>
-			<head>Portfolio</head>
+			<head>
+				<title>Portfolio - Alvaro S.</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</head>
 			<body>{children}</body>
 		</html>
 	);
