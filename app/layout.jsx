9f3f2b44d51019e3f6bce13c68f4482286e6b921
Alvarosanti <alvarosantisteban56@gmail.com>
2023-09-07 15:40:43 -0500

    fix loading img & layout name

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html>
 			<head>
-				<title>Portfolio - Alvaro S.</title>
+				<title>Portfolio - Alvaro Rodrigo</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</head>
 			<body>{children}</body>
